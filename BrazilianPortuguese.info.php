55e10d0e46630dc6da42c464bee96e961acf5f7b
Daniel Zilli <1160818+dlzi@users.noreply.github.com>
2024-08-22 21:07:43 -0300

    Initial commit

--- BrazilianPortuguese.info.php
+++
@@ -1,9 +0,0 @@
-<?php namespace ProcessWire;
-
-$info = array(
-    'title' => 'Brazilian Portuguese',
-    'summary' => 'Brazilian Portuguese language pack for ProcessWire',
-    'version' => 1,
-    'author' => 'dlzi',
-    'href' => 'https://github.com/dlzi/processwire-language-pt-br',
-);
